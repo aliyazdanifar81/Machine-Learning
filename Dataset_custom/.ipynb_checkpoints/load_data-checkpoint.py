3d2826d8a0eadbe3ab78d0d286fbb14bb30adb80
aliyazdanifar81 <yazdanifar1381@gmail.com>
2024-03-11 23:35:26 +0330

    update structure

---
+++ Dataset_custom/.ipynb_checkpoints/load_data-checkpoint.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
+class LoadModel:
+    def __init__(self):
+        self.paths = {
+            "house": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/houses.txt",
+            "Titanic_train": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/Titanic/train.csv",
+            "Titanic_test": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/Titanic/test.csv",
+            "Real_estate": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/Real estate.csv",
+            "ex2data2" : "/home/ali/Desktop/python/Machine Learning/Dataset_custom/ex2data2.txt"
+        }
+
+    def load_house(self, feature: int = 0):
+        with open(self.paths["house"]) as file:
+            x, y = [], []
+            for i in file:
+                i = list(map(float, i.split(sep=',')))
+                x.append(i[:feature])
+                y.append(i[-1])
+            x, y = np.array(x), np.array(y)
+        return x, y
+
+    def load_titanic(self):
+        x_train, y_train = self.__titanic_train()
+        x_test, simple_test = self.__titanic_test()
+        return x_train, y_train, x_test, simple_test
+
+    def load_realestate(self):
+        df = pd.read_csv(self.paths["Real_estate"])
+        x = df.drop(["No", "Y house price of unit area"], axis=1)
+        y = df["Y house price of unit area"]
+        return x, y
+
+    def load_ex2data2(self):
+        with open(self.paths["ex2data2"], 'r') as file:
+            x, y = [], []
+            for i in file:
+                i = list(map(float, i.split(sep=',')))
+                x.append(i[:2])
+                y.append(i[-1])
+            x, y = np.array(x), np.array(y)
+        return x, y
+
+    # Private Functions
+    def __titanic_train(self):
+        train = pd.read_csv(self.paths["Titanic_train"])
+        x_train, y_train = train.drop(["PassengerId", "Name"], axis=1), train["Survived"]
+        mode = x_train.mode().iloc[0]
+        mode["Cabin"] = "B96"
+        x_train = x_train.fillna(mode)
+        encoded = LabelEncoder()
+        x_train["Cabin"] = encoded.fit_transform(x_train["Cabin"])
+        x_train["Ticket"] = encoded.fit_transform(x_train["Ticket"])
+        x_train["Sex"] = encoded.fit_transform(x_train["Sex"])
+        x_train["Embarked"] = encoded.fit_transform(x_train["Embarked"])
+        return x_train, y_train
+
+    def __titanic_test(self):
+        test = pd.read_csv(self.paths["Titanic_test"])
+        simple = test
+        test = test.drop('Name', axis=1)
+        mode = test.mode().iloc[0]
+        mode["Cabin"] = "B57"
+        test = test.fillna(mode)
+        encoded = LabelEncoder()
+        test["Cabin"] = encoded.fit_transform(test["Cabin"])
+        test["Ticket"] = encoded.fit_transform(test["Ticket"])
+        test["Sex"] = encoded.fit_transform(test["Sex"])
+        test["Embarked"] = encoded.fit_transform(test["Embarked"])
+        return test, simple
--- Regressions/.ipynb_checkpoints/load_data-checkpoint.py
+++ Regressions/.ipynb_checkpoints/load_data-checkpoint.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def load_model(feature: int = 0):
-    with open("Dataset_custom/houses.txt") as file:
+    with open("./../Dataset_custom/houses.txt") as file:
         x, y = [], []
         for i in file:
             i = list(map(float, i.split(sep=',')))
@@ -10,4 +10,3 @@ def load_model(feature: int = 0):
             y.append(i[-1])
         x, y = np.array(x), np.array(y)
     return x, y
-
