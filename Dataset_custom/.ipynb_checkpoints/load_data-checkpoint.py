8c325b8d60619fb19396fd58a8e007dab1a8d128
aliyazdanifar81 <yazdanifar1381@gmail.com>
2024-03-27 01:11:59 +0330

    update structure

--- Dataset_custom/.ipynb_checkpoints/load_data-checkpoint.py
+++ Dataset_custom/.ipynb_checkpoints/load_data-checkpoint.py
@@ -10,7 +10,13 @@ class LoadModel:
             "Titanic_train": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/Titanic/train.csv",
             "Titanic_test": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/Titanic/test.csv",
             "Real_estate": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/Real estate.csv",
-            "ex2data2" : "/home/ali/Desktop/python/Machine Learning/Dataset_custom/ex2data2.txt"
+            "ex2data2": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/ex2data2.txt",
+            "ex2data1": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/ex2data1.txt",
+            "Cardio": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/Cardio/cardio_train.csv",
+            "hpart_train": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/house-prices-advanced-regression-techniques/train.csv",
+            # hpart stands for House Prices - Advanced Regression Techniques
+            "hpart_test": "/home/ali/Desktop/python/Machine Learning/Dataset_custom/house-prices-advanced-regression-techniques/test.csv"
+            # hpart stands for House Prices - Advanced Regression Techniques
         }
 
     def load_house(self, feature: int = 0):
@@ -24,9 +30,8 @@ class LoadModel:
         return x, y
 
     def load_titanic(self):
-        x_train, y_train = self.__titanic_train()
-        x_test, simple_test = self.__titanic_test()
-        return x_train, y_train, x_test, simple_test
+        train, test = pd.read_csv(self.paths["Titanic_train"]), pd.read_csv(self.paths["Titanic_test"])
+        return train, test
 
     def load_realestate(self):
         df = pd.read_csv(self.paths["Real_estate"])
@@ -44,30 +49,20 @@ class LoadModel:
             x, y = np.array(x), np.array(y)
         return x, y
 
-    # Private Functions
-    def __titanic_train(self):
-        train = pd.read_csv(self.paths["Titanic_train"])
-        x_train, y_train = train.drop(["PassengerId", "Name"], axis=1), train["Survived"]
-        mode = x_train.mode().iloc[0]
-        mode["Cabin"] = "B96"
-        x_train = x_train.fillna(mode)
-        encoded = LabelEncoder()
-        x_train["Cabin"] = encoded.fit_transform(x_train["Cabin"])
-        x_train["Ticket"] = encoded.fit_transform(x_train["Ticket"])
-        x_train["Sex"] = encoded.fit_transform(x_train["Sex"])
-        x_train["Embarked"] = encoded.fit_transform(x_train["Embarked"])
-        return x_train, y_train
+    def load_ex2data1(self):
+        with open(self.paths["ex2data1"], 'r') as file:
+            x, y = [], []
+            for i in file:
+                i = list(map(float, i.split(sep=',')))
+                x.append(i[:2])
+                y.append(i[-1])
+            x, y = np.array(x), np.array(y)
+        return x, y
 
-    def __titanic_test(self):
-        test = pd.read_csv(self.paths["Titanic_test"])
-        simple = test
-        test = test.drop('Name', axis=1)
-        mode = test.mode().iloc[0]
-        mode["Cabin"] = "B57"
-        test = test.fillna(mode)
-        encoded = LabelEncoder()
-        test["Cabin"] = encoded.fit_transform(test["Cabin"])
-        test["Ticket"] = encoded.fit_transform(test["Ticket"])
-        test["Sex"] = encoded.fit_transform(test["Sex"])
-        test["Embarked"] = encoded.fit_transform(test["Embarked"])
-        return test, simple
+    def load_cardio(self):
+        df = pd.read_csv(self.paths["Cardio"], sep=';')
+        return df
+
+    def load_hpart(self):
+        train, test = pd.read_csv(self.paths["hpart_train"]), pd.read_csv(self.paths["hpart_test"])
+        return train, test
